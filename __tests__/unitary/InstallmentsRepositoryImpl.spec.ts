9e11a856a7e54865ff664a5fec95d799dc265b1a
William Porto <william.porto@nave.rs>
2020-12-17 14:10:16 -0400

    tests: adapting repositories tests to mock datasources implementations

--- __tests__/unitary/InstallmentsRepositoryImpl.spec.ts
+++ __tests__/unitary/InstallmentsRepositoryImpl.spec.ts
@@ -1,34 +1,70 @@
+import { InstallmentsDataSource } from "../../src/datasources/interfaces/InstallmentsDataSource";
 import { InstallmentsStatusReport } from "../../src/dto/InstallmentsStatusReport";
+import { InstallmentStatus } from "../../src/entities/Installment";
 import { InstallmentsRepositoryImpl } from "../../src/repositories/implementations/InstallmentsRepositoryImpl";
 import { GenerateInstallmentsStatusReportUseCaseImpl } from "../../src/usecases/implementations/GenerateInstallmentsStatusReportUseCaseImpl";
 
 describe("Exercices with Installments Entity", () => {
+  const dataSource: InstallmentsDataSource = {
+    getInstallments: () => [
+      { id: 0, price: 10, status: InstallmentStatus.OPEN },
+      { id: 1, price: 20, status: InstallmentStatus.OPEN },
+      { id: 2, price: 30, status: InstallmentStatus.PAID },
+      { id: 3, price: 40, status: InstallmentStatus.PAID },
+    ],
+  };
+  const generateInstallmentsStatusReportUseCase = new GenerateInstallmentsStatusReportUseCaseImpl();
   const repository = new InstallmentsRepositoryImpl(
-    new GenerateInstallmentsStatusReportUseCaseImpl()
+    dataSource,
+    generateInstallmentsStatusReportUseCase
   );
 
   it("should be able to return the installments total price", () => {
-    expect(repository.getInstallmentsTotalPrice()).toBeCloseTo(703.08);
+    const installmentsTotalPriceFromRepository = repository.getInstallmentsTotalPrice();
+    const installmentsTotalPriceFromFakeDataSource = 10 + 20 + 30 + 40;
+    expect(installmentsTotalPriceFromRepository).toBeCloseTo(
+      installmentsTotalPriceFromFakeDataSource
+    );
   });
 
   it("should be able to return the installments status report", () => {
+    const installmentsStatusReportFromRepository = repository.getInstallmentsStatusReport();
+    const installmentStatusReportFromFakeDataSource: InstallmentsStatusReport = {
+      total_paid: 70,
+      total_open: 30,
+    };
     expect(
-      repository.getInstallmentsStatusReport()
-    ).toMatchObject<InstallmentsStatusReport>({
-      total_paid: 386.78,
-      total_open: 316.3,
-    });
+      installmentsStatusReportFromRepository
+    ).toMatchObject<InstallmentsStatusReport>(
+      installmentStatusReportFromFakeDataSource
+    );
   });
 
   it("should be able to return the installments ordered by price in descending", () => {
-    const installments = repository.getInstallmentsOrderedByPriceInDescending();
-    const prices = installments.map((installment) => installment.price);
-    expect(prices).toMatchObject([182.37, 139.88, 133.93, 123.45, 123.45]);
+    const installmentsOrderedByPriceInDesceningFromRepository = repository.getInstallmentsOrderedByPriceInDescending();
+    const installmentsOrderedByPriceInDesceningFromFakeDataSource = [
+      { id: 3, price: 40, status: InstallmentStatus.PAID },
+      { id: 2, price: 30, status: InstallmentStatus.PAID },
+      { id: 1, price: 20, status: InstallmentStatus.OPEN },
+      { id: 0, price: 10, status: InstallmentStatus.OPEN },
+    ];
+    expect(installmentsOrderedByPriceInDesceningFromRepository).toMatchObject(
+      installmentsOrderedByPriceInDesceningFromFakeDataSource
+    );
   });
 
   it("should be able to return the installments ordered by price in ascending with id as tiebraker criteria in descending", () => {
-    const installments = repository.getInstallmentsOrderedByPriceInAscendingWithIdAsTiebrakerCriteriaInDescending();
-    const ids = installments.map((installment) => installment.id);
-    expect(ids).toMatchObject([3, 1, 5, 2, 4]);
+    const installmentsOrderedByPriceInAscendingWithIdAsTiebrakerCriteriaInDescendingFromRepository = repository.getInstallmentsOrderedByPriceInAscendingWithIdAsTiebrakerCriteriaInDescending();
+    const installmentsOrderedByPriceInAscendingWithIdAsTiebrakerCriteriaInDescendingFromFakeDataSource = [
+      { id: 0, price: 10, status: InstallmentStatus.OPEN },
+      { id: 1, price: 20, status: InstallmentStatus.OPEN },
+      { id: 2, price: 30, status: InstallmentStatus.PAID },
+      { id: 3, price: 40, status: InstallmentStatus.PAID },
+    ];
+    expect(
+      installmentsOrderedByPriceInAscendingWithIdAsTiebrakerCriteriaInDescendingFromRepository
+    ).toMatchObject(
+      installmentsOrderedByPriceInAscendingWithIdAsTiebrakerCriteriaInDescendingFromFakeDataSource
+    );
   });
 });
